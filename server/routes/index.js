2fe02f1dbb105d9ad2c81fe98413e3f87bd51af0
milsoncodes <mr.sheppii@gmail.com>
2019-11-16 15:32:25 -0600

    api

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const Reports = require("../controllers/reports");
-
-module.exports = app => {
-  app.get("/api/reports", Reports.getReports);
-
-  app.post("/api/report", Reports.makeReport); // API route for reports
-};

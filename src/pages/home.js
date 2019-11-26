ea6bd47dad531c714835d3c57bcf365a4e8c7ff8
Brian Simpson <mojo_jo_jo3024@yahoo.com>
2019-11-26 16:11:19 -0600

    1st draft of Card, won't even build

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,10 @@
 import LinkComp from "../components/Link/index.js";
-
+import Card from "../components/Card"
 const Home = () => {
 	return (
 		<React.Fragment>
 			<h2>Welcome to Agit!</h2>
+			<Card title="Test" description="Description Test" restaurantName="Merchant" url="http://www.google.com"></Card>
 			<LinkComp to="/login">
         			<p>Login to Continue</p>
         		</LinkComp>

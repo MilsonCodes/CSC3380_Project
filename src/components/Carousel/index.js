d247166f19ad23d3af182d9218dd931231cff8e9
milsoncodes <mr.sheppii@gmail.com>
2019-12-06 13:28:37 -0600

    home page almost done

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Card from "../Card";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import { lighten } from "polished";
 
 let Container = styled.span`
   width: 80%;
@@ -9,7 +11,6 @@ let Container = styled.span`
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  overflow: hidden;
   padding: auto;
   margin: 25px;
 `;
@@ -20,18 +21,97 @@ let Span = styled.span`
   justify-content: center;
 `;
 
+let ChevronLeft = styled(FaChevronLeft)`
+  color: ${p => (p.left ? "black" : p.theme.background)};
+  display: ${p => (p.left ? "default" : "none")}
+  :hover {
+    color: ${p => (p.left ? lighten(0.2, "black") : p.theme.background)};
+    cursor: ${p => (p.left ? "pointer" : "auto")};
+  }
+`;
+
+let ChevronRight = styled(FaChevronRight)`
+  color: ${p => (p.right ? "black" : p.theme.background)};
+  display: ${p => (p.right ? "default" : "none")}
+  :hover {
+    color: ${p => (p.right ? lighten(0.2, "black") : p.theme.background)};
+    cursor: ${p => (p.right ? "pointer" : "auto")};
+  }
+`;
+
 const Carousel = props => {
-  let options = props.specials;
-  let cards = options.map(option => (
-    <React.Fragment>
-      <Card fields={option}></Card>
-    </React.Fragment>
-  ));
+  let fields = props.specials;
+  const size = 5;
+
+  const [options, switchOptions] =
+    fields.length > size
+      ? React.useState({
+          data: fields.slice(0, size),
+          right: true,
+          left: false,
+          offset: size
+        })
+      : React.useState({ data: fields, right: false, left: false, offset: 0 });
+
+  let moveRight = () => {
+    let move = options.data.length + 2 * size > fields.length ? false : true;
+    switchOptions({
+      data: fields.slice(options.offset, options.offset + size),
+      right: move,
+      left: true,
+      offset: options.offset + size
+    });
+    console.log(options.right);
+  };
+  let moveLeft = () => {
+    let move = options.data.length - 2 * options.offset < 0 ? false : true;
+    switchOptions({
+      data: fields.slice(options.offset - 2 * size, options.offset - size),
+      right: true,
+      left: move,
+      offset: options.offset - size
+    });
+  };
+
+  let Cards = () => {
+    return options.data.map(option => (
+      <React.Fragment>
+        <Card fields={option}></Card>
+      </React.Fragment>
+    ));
+  };
+
+  let Left = props => {
+    return props.left ? (
+      <span onClick={moveLeft}>
+        <ChevronLeft {...props}></ChevronLeft>
+      </span>
+    ) : (
+      <span>
+        <ChevronLeft {...props}></ChevronLeft>
+      </span>
+    );
+  };
+  let Right = props => {
+    return props.right ? (
+      <span onClick={moveRight}>
+        <ChevronRight {...props}></ChevronRight>
+      </span>
+    ) : (
+      <span>
+        <ChevronRight {...props}></ChevronRight>
+      </span>
+    );
+  };
   return (
     <React.Fragment>
       <h2>{props.title}</h2>
       <Span>
-        <Container>{cards}</Container>
+        <Container>
+          <Left left={options.left}></Left>
+          <Cards></Cards>
+          <Right right={options.right}></Right>
+        </Container>
       </Span>
     </React.Fragment>
   );

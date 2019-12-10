c5a5255e32cff5c171134177e49ad887fc145370
milsoncodes <mr.sheppii@gmail.com>
2019-12-09 21:09:18 -0600

    styling about

--- src/.next/server/static/development/pages/home.js
+++ src/.next/server/static/development/pages/home.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -2833,7 +2833,7 @@ let options1 = [special7, special8, special9];
 
 /***/ }),
 
-/***/ 5:
+/***/ 3:
 /*!*****************************!*\
   !*** multi ./pages/home.js ***!
   \*****************************/

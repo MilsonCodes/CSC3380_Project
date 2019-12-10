8e594e32c67fc9968e3844ecc486b9e16aee3a39
milsoncodes <mr.sheppii@gmail.com>
2019-12-09 21:21:11 -0600

    some styling

--- src/.next/server/static/development/pages/home.js
+++ src/.next/server/static/development/pages/home.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -2833,7 +2833,7 @@ let options1 = [special7, special8, special9];
 
 /***/ }),
 
-/***/ 3:
+/***/ 4:
 /*!*****************************!*\
   !*** multi ./pages/home.js ***!
   \*****************************/
